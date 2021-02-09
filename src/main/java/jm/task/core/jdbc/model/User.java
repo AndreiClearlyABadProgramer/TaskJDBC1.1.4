97414fab284a707808dba0ef8a18211852cb6d2f
Andrei <75860651+AndreiClearlyABadProgramer@users.noreply.github.com>
2021-02-09 09:50:17 +0300

    Update User.java

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 @Entity
 @Table(name = "Users")
 public class User {
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
